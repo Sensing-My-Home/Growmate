df2fafc8561559742b4f15708fb160e0eed6170d
abutuc <andrebutuc@ua.pt>
2023-03-20 15:44:38 +0000

    GM-27 initial docker-compose structure

---
+++ backend/src/test/java/pi/growmate/GrowmateApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.growmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
