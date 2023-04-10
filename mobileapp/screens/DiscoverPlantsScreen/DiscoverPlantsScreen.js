e6f75e46a1e28877a3cbd978e4765a8da5de3bde
abutuc <andrebutuc@ua.pt>
2023-04-10 14:51:22 +0100

    added CategoryScreenService and DiscoverPlantScreenService

--- mobileapp/screens/DiscoverPlantsScreen/DiscoverPlantsScreen.js
+++ mobileapp/screens/DiscoverPlantsScreen/DiscoverPlantsScreen.js
@@ -1,13 +1,20 @@
 import GreenBar from "../../components/GreenBar";
 import BottomMenu from "../../components/BottomMenu";
 import {Dimensions, View} from "react-native";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {useTheme} from "react-native-paper";
 import DiscoverNewPlantsHeader from "./components/DiscoverNewPlantsHeader";
 import DiscoverNewPlantsSearchBar from "./components/DiscoverNewPlantsSearchBar";
 import Categories from "./components/Categories";
+import {getCategories} from "../../service/DiscoverPlantsScreenService";
 
 export default function DiscoverPlantsScreen(){
+    const [categories, setCategories] = useState([]);
+
+    useEffect( () => {
+        getCategories().then((categories) => {setCategories(categories)});
+    }, []);
+
     const screenHeight = Dimensions.get('screen').height;
     const theme = useTheme()
     return (
@@ -15,7 +22,7 @@ export default function DiscoverPlantsScreen(){
             <GreenBar />
             <DiscoverNewPlantsHeader/>
             <DiscoverNewPlantsSearchBar/>
-            <Categories/>
+            <Categories categories={categories}/>
             <BottomMenu screenHeight={screenHeight} active={"magnify"} />
         </View>
     )
