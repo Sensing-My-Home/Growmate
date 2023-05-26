d79efa04e2e94b9f4477e7668a9c0f2da67ff027
abutuc <andrebutuc@ua.pt>
2023-05-26 12:56:03 +0100

    GM-153 done

--- mobileapp/screens/AddSensorScreen/AddSensorScreen.js
+++ mobileapp/screens/AddSensorScreen/AddSensorScreen.js
@@ -19,9 +19,10 @@ export default function AddSensorScreen(){
     const [sensorName, setSensorName] = useState("");
     const [sensorCode, setSensorCode] = useState("");
     const [sensorType, setSensorType] = useState(0);
-    const [ownerID, setOwnerID] = useState(0);
+    const [ownerID, setOwnerID] = useState(null);
     const [userPlants, setUserPlants] = useState([]);
     const [userDivisions, setUserDivisions] = useState([]);
+    const [codeIsValid, setCodeIsValid] = useState(true);
 
     useEffect( () => {
         getPlants(userID).then((plants) => {setUserPlants(plants)})
@@ -43,8 +44,9 @@ export default function AddSensorScreen(){
             <AddSensorDescription/>
             <SensorInfo sensorType={sensorType} plants={userPlants} divisions={userDivisions}
                         setSensorType={setSensorType} ownerID={ownerID} setOwnerID={setOwnerID}
-                        setSensorCode={setSensorCode} setSensorName={setSensorName}/>
-            <NextButton text={"CREATE"} reverse={true} onPress={onPressNext}/>
+                        setSensorCode={setSensorCode} setSensorName={setSensorName}
+                        codeIsValid={codeIsValid} setCodeIsValid={setCodeIsValid}/>
+            <NextButton text={"CREATE"} reverse={true} onPress={onPressNext} disabled={!(sensorCode.length > 0 && codeIsValid && sensorType !== null && ownerID !== null)}/>
             <BottomMenu screenHeight={screenHeight} active={"leaf"} />
         </View>
     )
